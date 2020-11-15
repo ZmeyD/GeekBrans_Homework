476ad56cba9ce98c136e79629a12175da8635fa6
NikitaKozlov <>
2020-11-16 02:37:04 +0300

    Complete First Task + add Second task

---
+++ FirstPractice/Second/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Second
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле I = m / (h * h); где m — масса тела в килограммах, h — рост в метрах.
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
