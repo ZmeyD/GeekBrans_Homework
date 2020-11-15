d95b610f2c61736b05e67587f3512ccb3eb7387c
NikitaKozlov <>
2020-11-16 00:36:50 +0300

    First Commit

---
+++ FirstPractice/FirstPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
