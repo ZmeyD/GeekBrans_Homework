852175ffdb90cb2c7916300a7eb76ff361a0cfac
NikitaKozlov <>
2020-11-16 05:38:43 +0300

    Complete First lesson: 1,2,3,4

---
+++ FirstPractice/Fourth/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Fourth
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Козлов Никита Сергеевич
+            //Написать программу обмена значениями двух переменных:
+
+
+            //а) с использованием третьей переменной;
+            Console.Write("x = ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("y = ");
+
+            int y = int.Parse(Console.ReadLine());
+
+
+            int z = x;        // В t запоминаем значение a
+            x = y;           // В a записываем b
+            y = z;
+
+
+            Console.Write($"Извините, но теперь переменная x = {x}, переменная y = {y} ");
+
+
+            Console.WriteLine("Еще разок...");
+            //б) *без использования третьей переменной.
+            Console.Write("a = ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b = ");
+
+            int b = int.Parse(Console.ReadLine());
+
+            
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+            Console.Write($"Извините, но теперь переменная a = {a}, переменная b = {b} ");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ FirstPractice/Second/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Second
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Козлов Никита Сергеевич
+            //Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле I = m / (h * h); где m — масса тела в килограммах, h — рост в метрах.
+            Console.Write("Введите ваш вес (кг): ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите ваш рост (метр): ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            double I = m / (h * h);
+            Console.WriteLine($"Ваш индекс массы тела(ИМТ) =  {I}");
+
+            Console.ReadLine();
+        }
+    }
+}
