d9c0eb1ca25d237cebfe1ca6fe23c7abd23ec738
NikitaKozlov <>
2020-11-16 03:10:33 +0300

    Add new practice

---
+++ FirstPractice/Fourth/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fourth
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Козлов Никита Сергеевич
+            //Написать программу обмена значениями двух переменных:
+            //а) с использованием третьей переменной;
+            //б) *без использования третьей переменной.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- FirstPractice/Second/Program.cs
+++ FirstPractice/Second/Program.cs
@@ -6,8 +6,18 @@ namespace Second
     {
         public static void Main(string[] args)
         {
+            //Козлов Никита Сергеевич
             //Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле I = m / (h * h); где m — масса тела в килограммах, h — рост в метрах.
-                Console.WriteLine("Hello World!");
+            Console.Write("Введите ваш вес (кг): ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите ваш рост (метр): ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            double I = m / (h * h);
+            Console.WriteLine($"Ваш индекс массы тела(ИМТ) =  {I}");
+
+            Console.ReadLine();
         }
     }
 }
