18ab710036c5f9f008b9eaac6284fc8d3acab45a
NikitaKozlov <>
2020-11-20 05:08:15 +0300

    6 example changed

--- Lesson2/Solution/6/Program.cs
+++ Lesson2/Solution/6/Program.cs
@@ -4,11 +4,45 @@ namespace Application
 {
     class MainClass
     {
+        //Козлов Н.С.
+        //*Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. «Хорошим» называется число, которое делится на сумму своих цифр. Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+        static bool IsGood(int x)
+        {
+            int i = x;
+            int sum = 0;
+
+            while (i > 0)
+            {
+                sum += i % 10;
+                i /= 10;
+            }
+            return i % sum == 0; ////возвращает в любом случае trur
+
+        }
+
         public static void Main(string[] args)
         {
-            //Kozlov N
-            //a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
-            //б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+            DateTime start = DateTime.Now;
+
+
+            int k = 0;
+            Console.WriteLine(IsGood(11));
+
+            for (int i = 10; i < 20; i++)
+
+                if (IsGood(i))
+                {
+                    k++;
+                    Console.WriteLine("{0} {1}", k, i);
+                }
+
+            Console.WriteLine(k);
+
+
+
+            Console.WriteLine("Время выполнения программы: ");
+            Console.WriteLine(DateTime.Now - start);
+            Console.ReadKey();
         }
     }
 }
