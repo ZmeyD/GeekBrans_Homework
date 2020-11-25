e67c237d3726f5ee28c4be48ec5b53db23a41ddf
NikitaKozlov <>
2020-11-25 19:03:22 +0300

    lesson 3 Homework Start

---
+++ Lesson3/Method_Structure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Method_Structure
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
