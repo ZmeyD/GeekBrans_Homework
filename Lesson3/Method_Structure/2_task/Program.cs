8113c12a7673985d30c3bdc0f6c153bdf4122168
NikitaKozlov <>
2020-11-26 00:59:20 +0300

    complete

--- Lesson3/Method_Structure/2_task/Program.cs
+++ Lesson3/Method_Structure/2_task/Program.cs
@@ -2,11 +2,40 @@
 
 namespace _task
 {
-    class MainClass
+
+
+
+
+    class Program
     {
-        public static void Main(string[] args)
+        static int value;
+        
+        static int GetValue()
         {
-            Console.WriteLine("Hello World!");
+            int a = 0;
+            int result = 0;
+
+            do
+            {
+                Console.Write("Введите числа, выходом будет число 0: ");
+                result = int.Parse(Console.ReadLine());
+                if (result > 0 && result % 2 == 1)
+                    a += result;
+            }
+            while (result != 0);
+
+            return a;
+        }
+     
+        static void Main()
+        {
+            
+            value = GetValue();
+            Console.WriteLine("Сумма положительны и нечетных = ", value);
+            
         }
     }
+
+
+
 }
